573ce92b2e6aaa20ee90bbde2d573c5bea44e03d
adj00080 <78515716+adj00080@users.noreply.github.com>
2024-09-15 19:16:15 -0500

    test

--- Inc/config.h
+++ Inc/config.h
@@ -421,7 +421,7 @@
 */
     #define DUAL_INPUTS                     // ADC*(Primary) + PWM(Auxiliary). Uncomment this to use Dual-inputs
   #ifdef DUAL_INPUTS
-    #define FLASH_WRITE_KEY       0x1005  // Flash memory writing key. Change this key to ignore the input calibrations from the flash memory and use the ones in config.h
+    #define FLASH_WRITE_KEY       0x1105  // Flash memory writing key. Change this key to ignore the input calibrations from the flash memory and use the ones in config.h
     #define CONTROL_GAMETRAK      1            // use ADC as input. Number indicates priority for dual-input. Disable CONTROL_SERIAL_USART2, FEEDBACK_SERIAL_USART2, DEBUG_SERIAL_USART2!
     #define CONTROL_PWM_RIGHT     0       // use RC PWM as input on the RIGHT cable. Number indicates priority for dual-input. Disable DEBUG_SERIAL_USART3!
     #define GAMETRAK_CONNECTION_NORMAL    // for normal wiring according to the wiki instructions
